7f97b46b344bd42285b38293354386ead19687ea
iNA <antonindrawijaya@gmail.com>
2024-12-19 04:59:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-let mybutton=document.getElementById("myBtn");window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop&gt;50||document.documentElement.scrollTop&gt;50){mybutton.style.display="block"}else{mybutton.style.display="none"}}
-function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0};const cursor=document.querySelector(".cursor");var timeout;document.addEventListener("mousemove",(e)=&gt;{let x=e.pageX;let y=e.pageY;cursor.style.top=y+"px";cursor.style.left=x+"px";cursor.style.display="block";function mouseStopped(){cursor.style.display="none"}clearTimeout(timeout);timeout=setTimeout(mouseStopped,5000)});document.addEventListener("mouseout",()=&gt;{cursor.style.display="none"});var prinshm1=window.location.toString();if(prinshm1.indexOf("?m=1","?m=1")&gt;0){var clean_nprinsh=prinshm1.substring(0,prinshm1.indexOf("?m=1"));window.history.replaceState({},document.title,clean_nprinsh)}
+let mybutton=document.getElementById("myBtn");window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>50||document.documentElement.scrollTop>50){mybutton.style.display="block"}else{mybutton.style.display="none"}}
+    function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0};
+  
+ const cursor=document.querySelector(".cursor");var timeout;document.addEventListener("mousemove",(e)=>{let x=e.pageX;let y=e.pageY;cursor.style.top=y+"px";cursor.style.left=x+"px";cursor.style.display="block";function mouseStopped(){cursor.style.display="none"}clearTimeout(timeout);timeout=setTimeout(mouseStopped,5000)});document.addEventListener("mouseout",()=>{cursor.style.display="none"});
+  
+
+    var prinshm1 = window.location.toString();
+    if (prinshm1.indexOf("?m=1","?m=1") > 0) {
+    var clean_nprinsh = prinshm1.substring(0, prinshm1.indexOf("?m=1"));
+    window.history.replaceState({}, document.title, clean_nprinsh);};
