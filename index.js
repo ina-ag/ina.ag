fa4e4c883adebc8e5377eafeb89ff4c6e98f82aa
iNA <antonindrawijaya@gmail.com>
2024-12-19 04:50:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-let mybutton=document.getElementById(&quot;myBtn&quot;);window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop&gt;50||document.documentElement.scrollTop&gt;50){mybutton.style.display=&quot;block&quot;}else{mybutton.style.display=&quot;none&quot;}}
-function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0};
-
-const cursor=document.querySelector(&quot;.cursor&quot;);var timeout;document.addEventListener(&quot;mousemove&quot;,(e)=&gt;{let x=e.pageX;let y=e.pageY;cursor.style.top=y+&quot;px&quot;;cursor.style.left=x+&quot;px&quot;;cursor.style.display=&quot;block&quot;;function mouseStopped(){cursor.style.display=&quot;none&quot;}clearTimeout(timeout);timeout=setTimeout(mouseStopped,5000)});document.addEventListener(&quot;mouseout&quot;,()=&gt;{cursor.style.display=&quot;none&quot;});
-
-
-var prinshm1 = window.location.toString();
-if (prinshm1.indexOf(&quot;?m=1&quot;,&quot;?m=1&quot;) &gt; 0) {
-var clean_nprinsh = prinshm1.substring(0, prinshm1.indexOf(&quot;?m=1&quot;));
-window.history.replaceState({}, document.title, clean_nprinsh);};
+let mybutton=document.getElementById("myBtn");window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop&gt;50||document.documentElement.scrollTop&gt;50){mybutton.style.display="block"}else{mybutton.style.display="none"}}
+function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0};const cursor=document.querySelector(".cursor");var timeout;document.addEventListener("mousemove",(e)=&gt;{let x=e.pageX;let y=e.pageY;cursor.style.top=y+"px";cursor.style.left=x+"px";cursor.style.display="block";function mouseStopped(){cursor.style.display="none"}clearTimeout(timeout);timeout=setTimeout(mouseStopped,5000)});document.addEventListener("mouseout",()=&gt;{cursor.style.display="none"});var prinshm1=window.location.toString();if(prinshm1.indexOf("?m=1","?m=1")&gt;0){var clean_nprinsh=prinshm1.substring(0,prinshm1.indexOf("?m=1"));window.history.replaceState({},document.title,clean_nprinsh)}
