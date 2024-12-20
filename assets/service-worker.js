6f53e50358f0b09e168cb394dc61e9134b9075a8
iNA <antonindrawijaya@gmail.com>
2024-12-20 17:47:41 +0700

    Update service-worker.js

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -1,42 +1,41 @@
-const CACHE_NAME = 'v1';
-const urlsToCache = [
-    'https://dev.ina.ag/index/offline.html',
-    
-];
-
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Caching files');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
+  event.waitUntil(
+    caches.open('ina-cache').then((cache) => {
+      return cache.addAll([
+        'https://www.ina.ag', 
+        'https:dev.ina.ag/css/ina.ag.style.css'
 
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((name) => {
-                    if (name !== CACHE_NAME) {
-                        console.log('Deleting old cache:', name);
-                        return caches.delete(name);
-                    }
-                })
-            );
-        })
-    );
+      ]);
+    })
+  );
 });
 
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-              
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+      
+        return cachedResponse;
+      }
+      
+      return fetch(event.request);
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['ina-cache'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
