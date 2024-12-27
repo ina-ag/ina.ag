d6e40aea12b1b387d3739286fd7df92782d6fe2d
iNA <antonindrawijaya@gmail.com>
2024-12-27 18:04:39 +0700

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,42 @@
- document.getElementById('inaHtml').addEventListener('input', function () {
-            var htmlContent = document.getElementById('inaHtml').value;
-            var iframe = document.getElementById('inaFrame');
-            var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-            
-            iframeDoc.open();
-            iframeDoc.write(htmlContent);
-            iframeDoc.close();
-        });
+document.getElementById('inaHtml').addEventListener('input', function () {
+    var htmlContent = document.getElementById('inaHtml').value;
+    var iframe = document.getElementById('inaFrame');
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    
+    iframeDoc.open();
+    iframeDoc.write(htmlContent);
+    iframeDoc.close();
+});
 
-        document.getElementById('saveBtn').addEventListener('click', function () {
-            var fileExtension = document.getElementById('fileExtension').value;
-            var filename = document.getElementById('filename').value || 'document';
-            var content = document.getElementById('inaHtml').value;
-            var blob, mimeType;
+document.getElementById('saveBtn').addEventListener('click', function () {
+    var fileExtension = document.getElementById('fileExtension').value;
+    var filename = document.getElementById('filename').value || 'document';
+    var content = document.getElementById('inaHtml').value;
+    var blob, mimeType;
 
-            switch (fileExtension) {
-                case 'html':
-                    mimeType = 'text/html';
-                    content = `<!DOCTYPE html><html lang="id"><head><meta charset="UTF-8"><title>${filename}</title></head><body>${content}</body></html>`;
-                    filename += '.html';
-                    break;
-                case 'css':
-                    mimeType = 'text/css';
-                    content = `/* CSS Content */\n${content}`;
-                    filename += '.css';
-                    break;
-                case 'js':
-                    mimeType = 'application/javascript';
-                    content = `// JavaScript Content\n${content}`;
-                    filename += '.js';
-                    break;
-                default:
-                    alert('Pilih ekstensi file yang valid!');
-                    return;
-            }
+    switch (fileExtension) {
+        case 'html':
+            mimeType = 'text/html';
+            filename += '.html';
+            break;
+        case 'css':
+            mimeType = 'text/css';
+            content = `/* CSS Content */\n${content}`;
+            filename += '.css';
+            break;
+        case 'js':
+            mimeType = 'application/javascript';
+            content = `// JavaScript Content\n${content}`;
+            filename += '.js';
+            break;
+        default:
+            alert('Pilih ekstensi file yang valid!');
+            return;
+    }
 
-            
-            var blob = new Blob([content], { type: mimeType });
-            var link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = filename;
-            link.click();
-        });
+    var blob = new Blob([content], { type: mimeType });
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+});
