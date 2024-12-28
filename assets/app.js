3f4146a6605ccf87eada3a0c972c9248cce959e0
iNA <antonindrawijaya@gmail.com>
2024-12-28 17:02:11 +0700

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,45 @@
+function applyGridStyles() {
+  const blogPosts = document.querySelector('.update-blog-posts');
+  
+  blogPosts.style.display = 'grid';
+  blogPosts.style.gridTemplateColumns = 'repeat(3, 1fr)';
+  blogPosts.style.gap = '15px';
+  blogPosts.style.textAlign = 'center';
+
+  const mediaQueries = [
+    {
+      media: '(max-width: 1024px)',
+      styles: {
+        gridTemplateColumns: 'repeat(2, 1fr)',
+      },
+    },
+    {
+      media: '(max-width: 768px)',
+      styles: {
+        gridTemplateColumns: 'repeat(1, 1fr)',
+      },
+    },
+    {
+      media: '(max-width: 480px)',
+      styles: {
+        gridTemplateColumns: 'repeat(1, 1fr)',
+      },
+    },
+  ];
+
+  function handleMediaQueries() {
+    mediaQueries.forEach((query) => {
+      if (window.matchMedia(query.media).matches) {
+        for (const property in query.styles) {
+          blogPosts.style[property] = query.styles[property];
+        }
+      }
+    });
+  }
+
+  handleMediaQueries();
+  window.addEventListener('resize', handleMediaQueries);
+}
+
+applyGridStyles();
 
