58755026f7f793adaccc8eb17de2ae30279442e5
iNA <antonindrawijaya@gmail.com>
2024-12-31 19:44:29 +0700

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -241,3 +241,31 @@ document.addEventListener('DOMContentLoaded', function() {
                 subcategory.style.display = 'block';
             }
         }
+
+
+// Scroll Anim
+
+const elements = document.querySelectorAll('.ina-anim');
+
+function checkVisibility() {
+    const windowHeight = window.innerHeight;
+
+    elements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        
+        
+        if (elementTop < windowHeight - 100) {
+            element.classList.add('visible');
+
+        } else {
+            element.classList.remove('visible');
+
+        }
+    });
+}
+
+
+window.addEventListener('scroll', checkVisibility);
+
+
+document.addEventListener('DOMContentLoaded', checkVisibility);
